77c45b3ff1aab04464a782b29bf806029edfd28f
Nnenna Onwuka <111025068+Nne1509@users.noreply.github.com>
2022-11-09 19:53:28 +0000

    new file

---
+++ number.py
@@ -0,0 +1,9 @@
+import random
+number = random.randint(-10, 10)
+if number > 0:
+  print (number, "number is positive")
+elif number < 0:
+  print (number, "number is negative")
+else:
+  print (number, "number is zero")
+  
