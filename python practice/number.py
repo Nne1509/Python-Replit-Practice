9db71e6fd66776f4f661a1ef526c5708e2d7c72b
Nne1509 <onwukannenna77@gmail.com>
2023-01-26 11:52:21 +0100

    implement change

--- python practice/number.py
+++ python practice/number.py
@@ -2,7 +2,7 @@ import random
 number = random.randint(-10, 10)
 if number > 0:
   print (number, "number is positive")
-elif number < 1:
+elif number < 0:
   print (number, "number is negative")
 else:
   print (number, "number is zero")
